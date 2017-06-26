cc0342438784cdd7105f8356e52dba7ba6ebd46f
rpanadero <rpanadero@okode.com>
2017-06-26 16:40:06 +0200

    Overriding sight call file provider to avoid integration conflicts

---
+++ src/android/SightcallFileProvider.java
@@ -0,0 +1,10 @@
+package com.okode.cordova.sightcall;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by rpanadero on 26/6/17.
+ */
+
+public class SightcallFileProvider extends FileProvider {
+}
