91634110ade76fadff690602a882d9e050e57c3d
rpanadero <rpanadero@okode.com>
2017-06-29 10:48:12 +0200

    Adding additional props that Google services plugin should add

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -30,6 +30,17 @@ function directoryExists(path) {
   }
 }
 
+var stripRegex = "([^\@<]+?)";
+function stripStringNonDefaultValue(strings, stringKey) {
+  if (strings == null) return;
+  return strings.replace(new RegExp('<string name="' + stringKey + '">' + stripRegex + '<\/string>', "i"), '');
+}
+
+function setValueToString(strings, stringKey, newValue) {
+  if (strings == null) return;
+  return strings.replace(new RegExp('<string name="' + stringKey + '">' + stripRegex + '<\/string>', "i"), '<string name="' + stringKey + '">' + newValue + '</string>')
+}
+
 var config = fs.readFileSync("config.xml").toString()
 var name = getValue(config, "name")
 
@@ -62,20 +73,29 @@ if (directoryExists("platforms/android")) {
         var json = JSON.parse(contents);
         var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
 
-        // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^\@<]+?)<\/string>', "i"), '')
-
-        // strip non-default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^\@<]+?)<\/string>', "i"), '')
+        var stripRegex = "([^\@<]+?)"
+        // strip non-default values
+        strings = stripStringNonDefaultValue(strings, google_app_id);
+        strings = stripStringNonDefaultValue(strings, google_api_key);
+        strings = stripStringNonDefaultValue(strings, gcm_defaultSenderId);
+        strings = stripStringNonDefaultValue(strings, default_web_client_id);
+        strings = stripStringNonDefaultValue(strings, ga_trackingId);
+        strings = stripStringNonDefaultValue(strings, firebase_database_url);
+        strings = stripStringNonDefaultValue(strings, google_crash_reporting_api_key);
 
         // strip empty lines
         strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
 
-        // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_app_id">([^<]+?)<\/string>', "i"), '<string name="google_app_id">' + json.client[0].client_info.mobilesdk_app_id + '</string>')
-
-        // replace the default value
-        strings = strings.replace(new RegExp('<string name="google_api_key">([^<]+?)<\/string>', "i"), '<string name="google_api_key">' + json.client[0].api_key[0].current_key + '</string>')
+        // replace the default values
+        strings = setValueToString(strings, google_app_id, json.client[0].client_info.mobilesdk_app_id);
+        strings = setValueToString(strings, google_api_key, json.client[0].api_key[0].current_key);
+        strings = setValueToString(strings, gcm_defaultSenderId, json.project_info.project_number);
+        strings = setValueToString(strings, default_web_client_id, json.client[0].oauth_client[0].client_id);
+        if (json.client[0].services.analytics_service.analytics_property != null) {
+          strings = setValueToString(strings, ga_trackingId, json.client[0].services.analytics_service.analytics_property.tracking_id);
+        }
+        strings = setValueToString(strings, firebase_database_url, json.project_info.firebase_url);
+        strings = setValueToString(strings, google_crash_reporting_api_key, json.client[0].api_key[0].current_key);
 
         fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
       } catch(err) {
