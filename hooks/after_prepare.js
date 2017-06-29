b8809810cc7cb3878632bd9e4a51397a7ce1a610
rpanadero <rpanadero@okode.com>
2017-06-29 11:13:51 +0200

    Fixed hook

--- hooks/after_prepare.js
+++ hooks/after_prepare.js
@@ -73,29 +73,28 @@ if (directoryExists("platforms/android")) {
         var json = JSON.parse(contents);
         var strings = fs.readFileSync("platforms/android/res/values/strings.xml").toString();
 
-        var stripRegex = "([^\@<]+?)"
         // strip non-default values
-        strings = stripStringNonDefaultValue(strings, google_app_id);
-        strings = stripStringNonDefaultValue(strings, google_api_key);
-        strings = stripStringNonDefaultValue(strings, gcm_defaultSenderId);
-        strings = stripStringNonDefaultValue(strings, default_web_client_id);
-        strings = stripStringNonDefaultValue(strings, ga_trackingId);
-        strings = stripStringNonDefaultValue(strings, firebase_database_url);
-        strings = stripStringNonDefaultValue(strings, google_crash_reporting_api_key);
+        strings = stripStringNonDefaultValue(strings, "google_app_id");
+        strings = stripStringNonDefaultValue(strings, "google_api_key");
+        strings = stripStringNonDefaultValue(strings, "gcm_defaultSenderId");
+        strings = stripStringNonDefaultValue(strings, "default_web_client_id");
+        strings = stripStringNonDefaultValue(strings, "ga_trackingId");
+        strings = stripStringNonDefaultValue(strings, "firebase_database_url");
+        strings = stripStringNonDefaultValue(strings, "google_crash_reporting_api_key");
 
         // strip empty lines
         strings = strings.replace(new RegExp('(\r\n|\n|\r)[ \t]*(\r\n|\n|\r)', "gm"), '$1')
 
         // replace the default values
-        strings = setValueToString(strings, google_app_id, json.client[0].client_info.mobilesdk_app_id);
-        strings = setValueToString(strings, google_api_key, json.client[0].api_key[0].current_key);
-        strings = setValueToString(strings, gcm_defaultSenderId, json.project_info.project_number);
-        strings = setValueToString(strings, default_web_client_id, json.client[0].oauth_client[0].client_id);
+        strings = setValueToString(strings, "google_app_id", json.client[0].client_info.mobilesdk_app_id);
+        strings = setValueToString(strings, "google_api_key", json.client[0].api_key[0].current_key);
+        strings = setValueToString(strings, "gcm_defaultSenderId", json.project_info.project_number);
+        strings = setValueToString(strings, "default_web_client_id", json.client[0].oauth_client[0].client_id);
         if (json.client[0].services.analytics_service.analytics_property != null) {
-          strings = setValueToString(strings, ga_trackingId, json.client[0].services.analytics_service.analytics_property.tracking_id);
+          strings = setValueToString(strings, "ga_trackingId", json.client[0].services.analytics_service.analytics_property.tracking_id);
         }
-        strings = setValueToString(strings, firebase_database_url, json.project_info.firebase_url);
-        strings = setValueToString(strings, google_crash_reporting_api_key, json.client[0].api_key[0].current_key);
+        strings = setValueToString(strings, "firebase_database_url", json.project_info.firebase_url);
+        strings = setValueToString(strings, "google_crash_reporting_api_key", json.client[0].api_key[0].current_key);
 
         fs.writeFileSync("platforms/android/res/values/strings.xml", strings);
       } catch(err) {
